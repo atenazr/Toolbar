c5641253ad90fa403228c38008db9397ef8c4428
atenazr <athenazare.teacher@gmail.com>
2022-03-30 19:29:18 +0430

    Upload simple reactive toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,212 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <div @keyup.space="action" tabindex="0" >
+
+    <div class="container">
+      <div class="result">
+        <div>
+          choseen tool : {{ choosenTool }}
+        </div>
+        <div>
+          selected mode : {{ mode }}
+        </div>
+      </div>
+
+      <div class="message">
+        <p>
+          {{ message }}
+        </p>
+      </div>
+    </div>
+
+    <div class="hand-icon" 
+         @click="changeMode"
+         :class="{'not-active': handActive}">
+      <i class="fas fa-hand-pointer"></i>
+    </div>
+
+    <div class="tool-bar">
+        <tool-bar
+        v-for="tool in toolSet"
+        :key="tool.name"
+        :item="tool"
+        @clickTool="clickTool"
+        ></tool-bar>
+    </div>
+
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import toolBar from "./components/toolBar.vue"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    toolBar
+  },
+  data(){
+    return{
+      toolSet:[
+        {
+          id:0,
+          name:'brush',
+          icon:"fas fa-paint-brush",
+          selected:false
+        },
+        {
+          id:1,
+          name:'magic pen',
+          icon:"fas fa-pen",
+          selected:false
+        },
+        {
+          id:2,
+          name:'eraser',
+          icon:"fas fa-eraser",
+          selected:false
+        },
+        {
+          id:3,
+          name:'pencil',
+          icon:"fas fa-pencil-alt",
+          selected:false
+        },
+        {
+          id:4,
+          name:'pick color',
+          icon:"fas fa-crosshairs",
+          selected:false
+        },
+        {
+          id:5,
+          name:'fill with color',
+          icon:"fas fa-fill-drip",
+          selected:false
+        }
+      ],
+      mode:'hand',
+      choosenTool:'',
+      choosenId:null,
+      message : 'select tools'
+    }
+  },
+  created(){
+    window.addEventListener('keyup', this.keyTool)
+    },
+  methods:{
+    keyTool(event) {
+      // console.log(event);
+      const shKey = event.key;
+      const shId = Number(shKey)-1;
+        if(shKey === "Escape"){
+          this.changeMode();
+        }
+        if(this.mode === "keyboard"){
+          if(shKey < this.toolSet.length+1){
+             this.selectTool(shId);
+          }else{
+            this.message = 'please select tools by shortkeys';
+          }
+          return
+        }else{
+          this.message = 'please change mode, then select tools';
+        }
+      },
+    clickTool(tool){
+      if(this.mode === 'hand'){
+        this.selectTool(tool.id);
+      }else{
+        this.message = 'please change mode or select tool by shortkeys';
+      }
+    },
+    selectTool(id){
+      this.message = "select tools";
+      this.choosenTool = this.toolSet[id].name;
+      if(!(this.choosenId === null)){
+        this.toolSet[this.choosenId].selected = false;
+      }
+      this.toolSet[id].selected = true;
+      this.choosenId = id;
+    },
+    changeMode(){
+      if(this.mode === 'keyboard'){
+            this.mode = 'hand';
+            this.message = "mode changed !... now select tool by hand";
+          }else{
+            this.mode = 'keyboard';
+            this.message = "mode changed !... now select tool by shortKeys";
+          }
+          return
+    }
+  },
+  computed:{
+    handActive(){
+      return this.mode !== 'hand';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: bold;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color:#EBD4CB;
 }
+*::selection{
+  color: #DA9F93;
+  background-color: #B6465F;
+}
+.container{
+ margin: 2rem auto;
+padding: .5rem;
+text-align: center;
+color: #2C0703;
+width: 50%;
+box-shadow: 2px 2px 10px #DA9F93;
+font-size: 1.1rem;
+}
+.result{
+  display: flex;
+  width: 100%;
+  padding: 1rem;
+}
+.result div{
+  margin: 0 auto;
+}
+.message{
+  padding: .5rem;
+  
+}
+.message p{
+  color: #890620;
+  font-weight: bold;
+  font-size: 1.4rem;
+}
+.hand-icon{
+  cursor: pointer;
+  padding: .2rem;
+  color: #890620;
+  font-weight: bold;
+  font-size: 1.4rem;
+  transition: .3s ease-in-out;
+}
+.hand-icon.not-active,
+.hand-icon:hover{
+  color: #DA9F93;
+}
+.tool-bar{
+  width:max-content;
+  display: flex;
+  flex-direction: column;
+  margin: .2rem auto;
+  padding: 1rem;
+  align-items: flex-start;
+}
+
 </style>
---
+++ src/components/toolBar.vue
@@ -0,0 +1,54 @@
+<template>
+
+    <div class="item">
+        <div @click="$emit('clickTool',Item)"
+        @mousemove="$emit('clickTool',Item)"
+        :class="{'select-item' : Item.selected }">
+            <i :class="Item.icon"></i>
+            <!-- <span>{{ Item.name }} </span> -->
+        </div>
+    </div> 
+
+</template>
+
+<script>
+export default {
+    props:[
+        'item'
+    ],
+    emits:[
+        'clickTool'
+    ],
+    data(){
+        return{
+            Item:this.item
+        }
+    },
+    computed:{
+        selectedOrNot(tmp){
+            return tmp.selected ? 'select-item' : ''
+        } 
+    }
+
+}
+</script>
+
+<style scoped>
+.item{
+    margin: .5rem;
+    padding: 0 .5rem;
+    cursor: pointer;
+    transition: .5s ease-in-out;
+    color: #890620;
+    border-left: 3px solid transparent;
+    border-right: 3px solid transparent ;
+    font-weight: bolder;
+}
+.item:hover,
+.select-item{
+    color: #B6465F;
+    border-color: #DA9F93;
+}
+
+
+</style>
